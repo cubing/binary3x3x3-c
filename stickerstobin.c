1af9368cb7abf1bf6c6231ad126a372b4fe6a02e
rokicki <rokicki@gmail.com>
2020-08-24 17:23:04 -0700

    Use a faster encode that doesn't have an inner loop but instead uses a lookup table to do popcount.

--- stickerstobin.c
+++ stickerstobin.c
@@ -97,19 +97,18 @@ static const unsigned short cornerExpand[] = { 19, 152, 194, 0, 28, 224,
 #endif
 /*
  *   Index a permutation.  Return -1 if all values are not seen.
- *   Zero based.  This can be made faster (no inner loop) if the
- *   CPU has a popcount instruction but some microcontrollers
- *   might not have such.
+ *   Zero based.
  */
+static const unsigned char popcount64[] = {
+   0,1,1,2,1,2,2,3, 1,2,2,3,2,3,3,4, 1,2,2,3,2,3,3,4, 2,3,3,4,3,4,4,5,
+   1,2,2,3,2,3,3,4, 2,3,3,4,3,4,4,5, 2,3,3,4,3,4,4,5, 3,4,4,5,4,5,5,6 } ;
 static int encodePerm(const unsigned char *a, int n) {
    int bits = 0 ;
    int r = 0 ;
    for (int i=0; i<n; i++) {
       bits |= 1<<a[i] ;
-      r = r * (n-i) ;
-      for (int j=i+1; j<n; j++)
-         if (a[i] > a[j])
-            r++ ;
+      int low = ((1<<a[i])-1) & bits ;
+      r = r * (n-i) + a[i] - popcount64[low>>6] - popcount64[low&63] ;
    }
    if (bits + 1 != 1 << n)
       return -1 ;
